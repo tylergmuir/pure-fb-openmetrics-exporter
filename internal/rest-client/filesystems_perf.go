557152c8d8958bcd8a4799a4258503cbe5cfe4c7
Tyler Muir <tylergmuir@gmail.com>
2025-12-31 08:51:42 -0800

    handle only fetching performance metrics for protocols that are enabled

--- internal/rest-client/filesystems_perf.go
+++ internal/rest-client/filesystems_perf.go
@@ -2,6 +2,12 @@ package client
 
 import "strings"
 
+const (
+	protocolAll = "all"
+	protocolNFS = "NFS"
+	protocolSMB = "SMB"
+)
+
 type FileSystemsPerformanceList struct {
 	CntToken     string        `json:"continuation_token"`
 	TotalItemCnt int           `json:"total_item_count"`
@@ -12,33 +18,51 @@ type FileSystemsPerformanceList struct {
 func (fb *FBClient) GetFileSystemsPerformance(f *FileSystemsList,
 	protocol string) *FileSystemsPerformanceList {
 	uri := "/file-systems/performance"
+	if protocol != protocolAll && protocol != protocolNFS && protocol != protocolSMB {
+		return &FileSystemsPerformanceList{}
+	}
 	result := new(FileSystemsPerformanceList)
 	const chunkSize = 10
 
+	var filesystems []FileSystem
 	switch protocol {
-	case "all", "NFS", "SMB":
-		for i := 0; i < len(f.Items); i += chunkSize {
-			names := make([]string, 0, chunkSize)
-			for _, fs := range f.Items[i:min(i+chunkSize, len(f.Items))] {
-				names = append(names, fs.Name)
+	case protocolNFS:
+		for _, fs := range f.Items {
+			if fs.Nfs.V3Enabled || fs.Nfs.V41Enabled {
+				filesystems = append(filesystems, fs)
 			}
-			n := strings.Join(names, ",")
-			temp := new(FileSystemsPerformanceList)
-			res, _ := fb.RestClient.R().
+		}
+	case protocolSMB:
+		for _, fs := range f.Items {
+			if fs.Smb.Enabled {
+				filesystems = append(filesystems, fs)
+			}
+		}
+	case protocolAll:
+		filesystems = f.Items
+	}
+
+	for i := 0; i < len(filesystems); i += chunkSize {
+		names := make([]string, 0, chunkSize)
+		for _, fs := range filesystems[i:min(i+chunkSize, len(filesystems))] {
+			names = append(names, fs.Name)
+		}
+		n := strings.Join(names, ",")
+		temp := new(FileSystemsPerformanceList)
+		res, _ := fb.RestClient.R().
+			SetResult(&temp).
+			SetQueryParam("names", n).
+			SetQueryParam("protocol", protocol).
+			Get(uri)
+		if res.StatusCode() == 401 {
+			fb.RefreshSession()
+			fb.RestClient.R().
 				SetResult(&temp).
 				SetQueryParam("names", n).
 				SetQueryParam("protocol", protocol).
 				Get(uri)
-			if res.StatusCode() == 401 {
-				fb.RefreshSession()
-				fb.RestClient.R().
-					SetResult(&temp).
-					SetQueryParam("names", n).
-					SetQueryParam("protocol", protocol).
-					Get(uri)
-			}
-			result.Items = append(result.Items, temp.Items...)
 		}
+		result.Items = append(result.Items, temp.Items...)
 	}
 	return result
 }
